8fdb54d79f1dc5705a5d56304e13ee32397a09e5
Hei <101058402+Heixxx@users.noreply.github.com>
2026-04-30 01:49:38 +0200

    fix: use inngest/next adapter (inngest/vercel not available in v3)

--- api/inngest/index.ts
+++ api/inngest/index.ts
@@ -1,12 +1,10 @@
-import { serve } from 'inngest/vercel';
+import { serve } from 'inngest/next';
 import { inngest } from '../../src/inngest/client';
 import { runCouncilFn } from '../../src/inngest/councilFn';
 
 export const config = { runtime: 'nodejs', maxDuration: 60 };
 
-const handler = serve({
+export default serve({
   client: inngest,
   functions: [runCouncilFn],
 });
-
-export default handler;
